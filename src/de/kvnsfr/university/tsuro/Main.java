5b827c81ad061d304d113380053cc320d1073133
Kevin J. Schaefer <mail@kevinschaefer.de>
2018-10-12 16:18:47 +0200

    Update Main.java

--- src/de/kvnsfr/university/tsuro/Main.java
+++ src/de/kvnsfr/university/tsuro/Main.java
@@ -1,8 +1,10 @@
 package de.kvnsfr.university.tsuro;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-		CardGenerator.getInstance().generateCards();
+		CardGenerator gc = CardGenerator.getInstance();
+		gc.generateCards();
+		System.out.println("No. unquie cards: " + gc.calculateUniqueCards());
 	}
 }
