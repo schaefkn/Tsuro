d390e64cbc9704933a54529bfa9fcab1f2caa862
Kevin J. Schaefer <mail@kevinschaefer.de>
2018-10-14 23:27:43 +0200

    Removed testCardEqualityFunctions from CardGenerator.java

--- src/de/kvnsfr/university/tsuro/CardGenerator.java
+++ src/de/kvnsfr/university/tsuro/CardGenerator.java
@@ -19,22 +19,6 @@ public class CardGenerator {
 		return instance;
 	}
 	
-	protected static void testCardEqualityFunctions() {
-		Card card = new Card();
-		card.connectNodesByIndex(0, 5);
-		card.connectNodesByIndex(1, 4);
-		card.connectNodesByIndex(2, 6);
-		card.connectNodesByIndex(3, 7);
-		
-		Card card2 = new Card();
-		card2.connectNodesByIndex(0, 4);
-		card2.connectNodesByIndex(1, 5);
-		card2.connectNodesByIndex(2, 7);
-		card2.connectNodesByIndex(3, 6);
-		
-		System.out.println("Card1 equals Card2 after rotation: " + card.equalsAfterRotation(card2));
-	}
-	
 	public void isEqual(int index1, int index2) {
 		if(cards.get(index1).equalsAfterRotation(cards.get(index2))) {
 			cards.get(index1).printCard();
