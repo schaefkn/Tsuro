7a0a0524c4422e139132d790b48524f6e6eceb26
Tobias Senger <senger.tobias@web.de>
2018-10-25 23:29:06 +0200

    Removed comments for class CheckCards

--- src/de/kvnsfr/university/tsuro/CheckCards.java
+++ src/de/kvnsfr/university/tsuro/CheckCards.java
@@ -1,14 +1,6 @@
 package de.kvnsfr.university.tsuro;
 
 public class CheckCards {
-	/**
-	 * 
-	 * @param connections A two dimensional array that contains all pairs of connections
-	 * @param edges The total number of edges per card
-	 * @param offset A specific offset integer value for the amount of edges to iterate on
-	 * @param value A specific integer value to check for an existing connection with
-	 * @return true if a connection specified by the given value is still available
-	 */
 	public static boolean getAvailability(int[][] connections, int edges, int offset, int value) {		
 		for(int i = 0; i < edges - offset; i++) {
 			for(int j = 0; j < 2; j++) {
