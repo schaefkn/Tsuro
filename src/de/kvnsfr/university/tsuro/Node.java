b02a1b0bc6748f2e5a86b991dad6e02210d39f60
Tobias Senger <senger.tobias@web.de>
2018-10-25 23:44:50 +0200

    Added comments for classes Node and Position

--- src/de/kvnsfr/university/tsuro/Node.java
+++ src/de/kvnsfr/university/tsuro/Node.java
@@ -5,16 +5,30 @@ public class Node {
 	Edge edge;
 	boolean connected = false;
 	
+	/**
+	 * Another constructor for class Node
+	 * @param pos Object from class Position that specifies the position of this node
+	 */
 	public Node(Position pos) {
 		this.pos = pos;
 		this.edge = null;
 	}
 	
+	/**
+	 *  Another constructor for class Node
+	 * @param x integer value that specifies the x position of this node
+	 * @param y integer value that specifies the y position of this node
+	 */
 	public Node(int x, int y) {
 		this.pos = new Position(x, y);
 		this.edge = null;
 	}
 	
+	/**
+	 * Connects this node with another given node if connection doesn't already exist by creating a new edge
+	 * @param nodeToConnect Object from class Node to connect this node with
+	 * @return true if the connection was successfully created
+	 */
 	public boolean connectNode(Node nodeToConnect) {
 		if(!this.connected && !nodeToConnect.connected) {
 			this.edge = new Edge(this, nodeToConnect);
@@ -27,6 +41,11 @@ public class Node {
 		return false;
 	}
 	
+	/**
+	 * Checks whether this node equals a given node by comparing their position values
+	 * @param nodeToCompare Object from class Node to compare with
+	 * @return true if the given node matches this node
+	 */
 	public boolean equals(Node nodeToCompare) {
 		if(this.pos.equals(nodeToCompare.pos))
 			return true;
--- src/de/kvnsfr/university/tsuro/Position.java
+++ src/de/kvnsfr/university/tsuro/Position.java
@@ -4,11 +4,21 @@ public class Position {
 	int x;
 	int y;
 	
+	/**
+	 * Another constructor for class Position
+	 * @param x integer value that specifies the x value of the position
+	 * @param y integer value that specifies the y value of the position
+	 */
 	public Position(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 	
+	/**
+	 * Checks whether this position equals another given position
+	 * @param posToCompare Object from class Position to compare with
+	 * @return true if the given position equals this position
+	 */
 	public boolean equals(Position posToCompare) {
 		if(this.x == posToCompare.x && this.y == posToCompare.y)
 			return true;
