edba2101ae3010b5ad90b670d87af47526a99ed0
Tobias Senger <senger.tobias@web.de>
2018-10-25 23:35:41 +0200

    Added comments for the class Edge

--- src/de/kvnsfr/university/tsuro/Edge.java
+++ src/de/kvnsfr/university/tsuro/Edge.java
@@ -4,15 +4,29 @@ public class Edge {
 	Node origin;
 	Node dest;
 	
+	/**
+	 * Another constructor for class Edge
+	 * @param origin The origin node connected with the destination node
+	 * @param dest The destination node that is connected to by the origin node
+	 */
 	public Edge(Node origin, Node dest) {
 		this.origin = origin;
 		this.dest = dest;
 	}
 	
+	/**
+	 * Reverses this edge by flipping origin and dest node of the edge
+	 * @return new object from class Edge that equals this edge reversed
+	 */
 	public Edge reversedEdge() {
 		return new Edge(this.dest, this.origin);
 	}
 	
+	/**
+	 * Checks whether the given edge equals this edge by checking the containing nodes normally and reversed
+	 * @param e An object from class Edge to check for
+	 * @return true if the given edge equals this edge normally or reversed
+	 */
 	public boolean equals(Edge e) {
 		Edge reversed = this.reversedEdge();
 		
